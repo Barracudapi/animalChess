4344b77e21e5abab6c7c9f021c5883d761ad9593
Harrold <harroldtok@gmail.com>
2023-05-04 21:19:43 +0800

    Create Dog.java

---
+++ game/src/Dog.java
@@ -0,0 +1,20 @@
+public class Dog extends Piece {
+    public Dog(Color color, String name, int animalPower){
+        super(color, name, animalPower);
+    }
+    @Override
+    public boolean canMove(Board board, Spot start, Spot end) {
+        if(end.getSpotType().toString() == "BASERED" && start.getPiece().getColor().toString() == "RED") return false;
+        if(end.getSpotType().toString() == "BASEYELLOW" && start.getPiece().getColor().toString() == "YELLOW") return false;
+        if(end.getSpotType().toString() == "Water") return false;
+        if((start.getX() == end.getX() && Math.abs(start.getY()-end.getY()) == 1) || (start.getY() == end.getY() && Math.abs(start.getX()-end.getX()) == 1)){
+            if(end.getPiece() == null){
+                return true;
+            }
+            else if(end.getPiece().getAnimalPower()<= start.getPiece().getAnimalPower()){
+                return true;
+            }
+        }
+        return false;
+    }
+}
