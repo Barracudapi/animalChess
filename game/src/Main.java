975bb3860a877915aaf7fa8a94a0a62e8b7f19e6
Harrold <harroldtok@gmail.com>
2023-05-09 17:04:08 +0800

    3

--- game/src/Main.java
+++ game/src/Main.java
@@ -5,11 +5,11 @@ public class Main {
         Board b = new Board();
         Player player1 = new Player("P1", Piece.Color.RED);
         Player player2 = new Player("P2", Piece.Color.YELLOW);
-        Spot spot1 = new Spot(3, 1, null, Spot.Type.LAND);
+        Spot spot1 = new Spot(1, 2, null, Spot.Type.LAND);
         spot1.setPiece(b.getSpot(spot1).getPiece());
-        Spot spot2 = new Spot(4, 1, null, Spot.Type.LAND);
+        Spot spot2 = new Spot(1, 3, null, Spot.Type.LAND);
         spot2.setPiece(b.getSpot(spot2).getPiece());
-        Move move = new Move(player1, spot2, spot1);
+        Move move = new Move(player1, spot1, spot2);
         b.movePiece(move);
         b.printBoard();
     }
