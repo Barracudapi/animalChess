1253b75357b46760d7b3a22f5492a1b85269db50
Saruul <saruul@Saruuls-MacBook-Pro.local>
2023-05-11 15:12:03 +0800

    added piece color

--- game/src/PiecePanel.java
+++ game/src/PiecePanel.java
@@ -14,13 +14,19 @@ public class PiecePanel extends JPanel {
         this.piece = piece;
 
         // set the layout to BorderLayout
-        setPreferredSize(new Dimension(80, 80));
-        setBackground(Color.WHITE);
-        setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        setPreferredSize(new Dimension(60, 60));
+        
         setLayout(new BorderLayout());
-
+        
         // create a new JLabel with the ImageIcon and add it to the center of the panel
         JLabel label = new JLabel(piece.getName());
+        if(piece.getColor()==Piece.Color.RED){
+            // label.setForeground(Color.RED);
+            setBorder(BorderFactory.createLineBorder(Color.RED, 5));
+        } else{
+            // label.setForeground(Color.YELLOW);
+            setBorder(BorderFactory.createLineBorder(Color.cyan, 5));
+        }
         label.setHorizontalAlignment(JLabel.CENTER);
         label.setVerticalAlignment(JLabel.CENTER);
         add(label, BorderLayout.CENTER);
